40e790153f950da9ab014d44fc2480d05de54d70
Nick <nicksbpittman@gmail.com>
2015-02-16 17:57:37 -0500

    testing

--- index.py
+++ index.py
@@ -1,17 +1,2 @@
 __author__ = 'rawaid'
-import sqlite3
-class SearchDB:
-    def __init__(self, dbfile):
-        self.dbfile = dbfile
-        self.cxn = sqlite3.connect(dbfile)
-        self.cur = self.cxn.cursor()
 
-    def getClean(self):
-
-    def getTitle(self):
-
-    def getURL(self):
-
-    def getItemType(self):
-
-    def getItemName(self):
