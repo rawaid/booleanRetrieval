e528e8bc4486aa35e441165bfaebc1e6ad4b1fd7
Nick <nicksbpittman@gmail.com>
2015-02-18 16:29:22 -0500

    testing

--- dbsandbox.py
+++ dbsandbox.py
@@ -15,6 +15,18 @@ def get_urls():
     for record in cur.fetchall():
         print(record)
 
+def get_clean(item):
+    #cur.execute("SELECT id FROM CachedURL WHERE url='%s'" % urlIn)
+    #item = cur.fetchone()
+    item = str(item)
+    file = open("data/clean/"+item+".txt")
+    cleanText = file.read()
+    #print(item)
+    #print(cleanText)
+    return cleanText
 
-get_items()
-get_urls()
+
+
+#get_items()
+#get_urls()
+get_clean(4)
