d6af1bc02556466de7b2f6986ffe0e11249e6675
Nick <nicksbpittman@gmail.com>
2015-02-19 16:32:31 -0500

    added stuff

--- booleanRetrieval.py
+++ booleanRetrieval.py
@@ -1,1 +1,31 @@
 __author__ = 'rawaid'
+
+from index import make_index
+
+def ui():
+    indie = make_index()
+    print("Choose Query Type:\n1: Single Token Query\n2: AND Query\n3: OR Query",
+           "\n4: Phrase Query\n5: Near Query\n6: QUIT")
+    uRes = input("Enter Query Type (1-6): ")
+    uRes = int(uRes)
+    if uRes == 1:
+        query = input("Please enter a single word: ")
+    elif uRes == 2:
+        fWord = input("Please enter first word: ")
+        sWord = input("Please enter second word: ")
+    elif uRes == 3:
+        fWord = input("Please enter first word: ")
+        sWord = input("Please enter second word: ")
+    elif uRes == 4:
+        query = input("Please enter phrase: ")
+    elif uRes == 5:
+        fWord = input("Please enter first word: ")
+        sWord = input("Please enter second word: ")
+    elif uRes == 6:
+        print("Thank you and have a nice day!")
+
+    else:
+        print("Try picking an option this time.  Did you think ", uRes, " was an option?\n\n")
+        ui()
+
+ui()
--- index.py
+++ index.py
@@ -33,53 +33,6 @@ def make_index():
 #added comment
 make_index()
 
-# class InvertedIndexModel:
-#
-#     # new basic dictionary
-#     #invertedIndex = defaultdict(defaultdict())
-#
-#     def __init__(self, docID):
-#         self.invertedIndex = defaultdict(lambda : defaultdict())
-#         self.docID = docID
-#         self.populateInvertedIndex()
-#
-#
-#     def populateInvertedIndex(self):
-#         tokenList = self.getTokensFromCleanFile()
-#         return tokenList
-#
-#     def getTokensFromCleanFile(self):
-#         fileName = self.getFileName()
-#         fo = open(("data/clean/" + fileName), "r", encoding='latin1')
-#
-#         term = fo.readline()
-#         i = 0
-#         for term in fo:
-#             if term not in self.invertedIndex:
-#                 self.invertedIndex[term] = defaultdict(list)
-#             if self.docID not in self.invertedIndex[term]:
-#                 self.invertedIndex[term][self.docID].append(i)
-#             i = i+1
-#
-#         fo.close()
-#         return self.invertedIndex
-#
-#     def getFileName(self):
-#         filename = str(self.docID)
-#         # nameLength = len(filename)
-#         # for i in range(abs(nameLength - 7)):
-#         #     filename = "0" + filename
-#
-#         return filename + ".txt"
-#
-#
-# def main():
-#     for i in range(1,2):
-#         docID = str(i)
-#         ind = InvertedIndexModel(docID)
-#         token = ind.populateInvertedIndex()
-#         print(token)
-#
-# main()
+
 
 
