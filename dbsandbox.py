5ee42097646761b4479fa587783a30d7d485bff9
rawaid <rawaidakhtar@gmail.com>
2015-02-19 18:06:46 -0500

    made getters return instead of print

--- dbsandbox.py
+++ dbsandbox.py
@@ -31,25 +31,25 @@ def get_title(idIn):
     #use id to lookup title in CachedURL
     cur.execute("SELECT title FROM CachedURL WHERE id = '%i'" % idIn)
     for record in cur.fetchall():
-        print(record)
+        return(record)
 
 def get_url(idIn):
     #use id to lookup url in CachedURL
     cur.execute("SELECT url FROM CachedURL WHERE id = '%i'" % idIn)
     for record in cur.fetchall():
-        print(record)
+        return(record)
 
 def get_type(idIn):
     #use item table to get type of item
     cur.execute("SELECT type FROM Item WHERE id = '%i'" % idIn)
     for record in cur.fetchall():
-        print(record)
+        return(record)
 
 def get_item(idIn):
     #use item table to get the name
     cur.execute("SELECT name FROM Item WHERE id = '%i'" % idIn)
     for record in cur.fetchall():
-        print(record)
+        return(record)
 
 
 
@@ -58,7 +58,7 @@ def get_item(idIn):
 #get_items()
 #get_urls()
 #get_clean(4)
-get_title(1)
-get_url(1)
-get_type(1)
-get_item(1)
+#get_title(1)
+#get_url(1)
+#get_type(1)
+#get_item(1)
