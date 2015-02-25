a80e6079b75e23299d133639cf89c0b85a6d9116
Nick <nicksbpittman@gmail.com>
2015-02-25 15:45:10 -0500

    added stemmer to query

--- index.py
+++ index.py
@@ -2,19 +2,16 @@ __author__ = 'rawaid'
 
 from dbsandbox import get_clean
 from dbsandbox import get_size
-from collections import defaultdict
 #from dbsandbox import
 
 def make_index():
-    i = True
     fileNum = 1
-    file = 1
     index = dict()
     numURL = get_size()
     while fileNum < numURL:
         cleanText = get_clean(fileNum)
         #print(cleanText)
-        wordInd = 0
+        wordInd = 1
         docID = fileNum
         for item in cleanText:
             if item not in index:
@@ -25,10 +22,6 @@ def make_index():
             index[item][docID].append(wordInd)
             wordInd += 1
 
-        if cleanText is None:
-            i = False
-        else:
-            file += 1
         fileNum += 1
     #print(index)
     return index
